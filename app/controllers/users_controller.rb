2151dce83004aff71ba69f7b6dffdacf7ee39ec8
Chris Minns <chrism465@gmail.com>
2021-11-19 12:49:34 +0000

    pre-luncheon just in case, also search bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,20 @@ class UsersController < ApplicationController
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
+	def search
+
+		if params[:terms].present?
+			@user = User.new(User.where(email: params[:terms])).first
+			byebug
+			if params[:terms].include?("@")
+				
+			else
+
+			end
+		end
+	end
 	def my_friends
 		@friends = current_user.friends
 	end
+
 end
